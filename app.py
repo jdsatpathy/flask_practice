ccc12f960ddd1ce8172c07d3bc1a239746a76141
Jd <jdsatpathy@gmail.com>
2025-10-19 14:20:00 +0530

    lot of things

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import DecimalField, RadioField, SelectField, TextAreaField, FileField
+from wtforms.validators import InputRequired
+from werkzeug.security import generate_password_hash
+import traceback
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+
+
+class MyForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    submit = SubmitField('Submit', validators=None)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route('/form_page')
+def form_page():
+    form = MyForm()
+    return render_template('form.html', form=form)
+
+
+@app.route('/form_submit', methods=['GET', 'POST'])
+def on_form_submit():
+    form = MyForm()
+
+    if form.validate_on_submit():
+        return render_template('form_result.html', form=form)
+
 
 
 @app.route('/link1')
